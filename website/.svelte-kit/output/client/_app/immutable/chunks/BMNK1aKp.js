2955fb9fe52aab2d78e78c77888d04f7799b8430
Henryoman <henryoman@mac.com>
2026-02-24 22:07:51 -0800

    chore: ignore and untrack generated frontend build artifacts

--- website/.svelte-kit/output/client/_app/immutable/chunks/BMNK1aKp.js
+++
@@ -1,1 +0,0 @@
-import{o as a}from"./Cf1zhldJ.js";a();
--- website/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DNmWlo4j.js","_app/immutable/chunks/C4o7FmYS.js","_app/immutable/chunks/Cf1zhldJ.js","_app/immutable/chunks/J_YLyXvo.js"];
-export const stylesheets = ["_app/immutable/assets/0.Bmrz5L_u.css"];
-export const fonts = [];
